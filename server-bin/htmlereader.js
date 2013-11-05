7334e5342194d52efd4ff0696602c2943a1393fe
Jeff <jeff@blindferret.com>
2013-11-05 09:41:02 -0500

    Upload server hopefully working

--- server-bin/htmlereader.js
+++ server-bin/htmlereader.js
@@ -1,13 +1,24 @@
 var http = require('http');
 var fs = require('fs');
+var uuid = require('node-uuid');
 
 http.createServer(function(req, res) {
     console.log(req.method);
     if (req.method == "POST") {
         req.on('data', function(data) {
             console.log(data);
+            var saveLocation = './icons/' + uuid.v1() + '.jpg';
+            var ws = fs.createWriteStream(saveLocation);
+            ws.on('error', function(err) {
+                console.log("Error saving icon file - " + err);
+                res.end("Error during saving");
+            });
+            ws.on('close', function() {
+                console.log("Success");
+                res.end(saveLocation);
+            });
+            ws.end(data);
         });
-        res.end("Huzzah!");
     } else {
         res.end("Post requests only");
     }
